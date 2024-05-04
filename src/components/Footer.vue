9ca20b6a6bfdf8a6af26af4c35aed85f0b49cc05
kimdw524 <kimdw524@naver.com>
2024-05-05 00:39:57 +0900

    feat: Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.container">
+    <div :class="$style.strong">WhereIsMyHome - 구미 5반 13팀</div>
+  </div>
+</template>
+
+<style module>
+.container {
+  padding: 1.5rem 2rem;
+
+  background-color: var(--footer-bg);
+}
+.strong {
+  color: #d6d8db;
+  font-weight: 300;
+}
+</style>
