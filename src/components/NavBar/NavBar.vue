ccb73fdfc6240642c35468dfcdd65b7cdbb39174
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-05-07 17:28:03 +0900

    feat: NavBar

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import NavLink from './NavLink.vue';
+
+const route = useRoute();
+
+const navLinks = [
+  { name: 'Home', slug: '/home' },
+  { name: '공지사항', slug: '/notice' },
+];
+</script>
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.logo">
+      <RouterLink to="/">WhereIsMyHome</RouterLink>
+    </div>
+    <div>
+      <NavLink
+        v-for="link in navLinks"
+        :key="link.slug"
+        :slug="link.slug"
+        :active="link.slug == route.path"
+      >
+        {{ link.name }}
+      </NavLink>
+    </div>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 3.5rem;
+  padding: 0.5rem 1rem;
+  border-bottom: 1px solid #ededed;
+}
+
+.logo {
+  font-size: 0.625rem;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+</style>
---
+++ src/components/NavBar/NavLink.vue
@@ -0,0 +1,40 @@
+<script setup>
+const props = defineProps({
+  slug: String,
+  active: Boolean,
+});
+</script>
+
+<template>
+  <RouterLink :to="slug">
+    <div :class="[$style.container, active && $style.active]"><slot /></div>
+  </RouterLink>
+</template>
+
+<style module>
+.container {
+  display: inline-block;
+
+  padding: 0.75rem 1rem;
+
+  background-color: transparent;
+
+  border-radius: 0.5rem;
+
+  color: var(--color);
+  font-weight: 500;
+  text-decoration: none;
+
+  transition: all 200ms ease;
+
+  user-select: none;
+}
+
+.container.active {
+  color: #4260ff;
+}
+
+.container:hover {
+  background-color: #f5f5f5;
+}
+</style>
