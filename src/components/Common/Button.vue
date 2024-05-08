ed82841593ce6c8260793e0e41405ed1cf5252aa
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-05-08 16:23:07 +0900

    feat: Button Component

---
+++ src/components/Common/Button.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  variant: {
+    type: String,
+    default: 'primary',
+  },
+});
+</script>
+
+<template>
+  <button :class="[$style.button, $style[variant]]"><slot /></button>
+</template>
+
+<style module>
+.button {
+  padding: 0.75rem 1.25rem;
+
+  background-color: var(--bg);
+
+  border: 0;
+  border-radius: 0.25rem;
+
+  color: var(--color);
+  font-weight: 500;
+  font-size: 0.875rem;
+  font-family: inherit;
+  text-decoration: none;
+  letter-spacing: 1px;
+
+  cursor: pointer;
+  outline: none;
+}
+
+.button:hover {
+  background-color: var(--bg-hover);
+}
+
+.primary {
+  --bg: var(--button-primary-bg);
+  --bg-hover: var(--button-primary-bg-hover);
+  --color: var(--button-primary-color);
+}
+</style>
