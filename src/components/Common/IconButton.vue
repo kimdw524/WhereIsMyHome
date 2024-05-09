ea48e94fb7d3372ee958c1a39603f8c25c522d99
kimdw524 <kimdw524@naver.com>
2024-05-10 01:02:57 +0900

    feat: IconButton Component

---
+++ src/components/Common/IconButton.vue
@@ -0,0 +1,51 @@
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button :class="$style.button">
+    <span :class="$style.icon">
+      <slot />
+    </span>
+    <span>{{ text }} </span>
+  </button>
+</template>
+
+<style module>
+.button {
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  padding: 0.75rem 0.5rem;
+
+  background-color: transparent;
+
+  border: 0;
+
+  color: var(--color);
+  font-weight: 400;
+  font-size: 1rem;
+  font-family: inherit;
+  text-decoration: none;
+
+  cursor: pointer;
+  outline: none;
+  user-select: none;
+}
+
+.icon {
+  margin-right: 0.5rem;
+
+  transition: all 200ms ease;
+}
+
+.button:hover .icon {
+  transform: translateX(-0.25rem);
+}
+</style>
