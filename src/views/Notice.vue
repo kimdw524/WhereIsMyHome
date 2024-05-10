ee2414d0abbab946c7fcfcfe0d02c46373c39fc6
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-05-10 17:04:59 +0900

    Update Notice.vue

--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -1,5 +1,15 @@
-<script setup></script>
+<script setup>
+const posts = [{ title: '', content: '', updatedAt: '' }];
+</script>
 
-<template>Notice</template>
+<template>
+  <div :class="$style.container">
+    <h1>공지사항</h1>
+  </div>
+</template>
 
-<style module></style>
+<style module>
+.container {
+  padding: 1rem;
+}
+</style>
