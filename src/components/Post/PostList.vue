e1f5eb4fb71ff69c3a841b2fbe1e86196224dc75
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-05-10 17:05:30 +0900

    feat: PostList

---
+++ src/components/Post/PostList.vue
@@ -0,0 +1,96 @@
+<script setup>
+const props = defineProps({
+  data: Object,
+});
+
+const dateFormat = (time) => {
+  return new Date(time * 1000).toLocaleString('ko', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hourCycle: 'h24',
+  });
+};
+</script>
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.title">
+      {{ data.title }}
+    </div>
+    <div :class="$style.info">
+      <div>
+        <span :class="$style.name">{{ data.name }}</span>
+        <span :class="$style.date">{{ dateFormat(data.updatedAt) }}</span>
+      </div>
+      <div>
+        <span :class="$style.viewCount"
+          ><font-awesome-icon :icon="['far', 'eye']" />{{ data.viewCount }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<style module>
+.container {
+  margin: 0 1rem;
+  padding: 1rem 0.5rem;
+  border-bottom: 1px solid #e7eaee;
+
+  background-color: rgb(255, 255, 255);
+
+  cursor: pointer;
+}
+
+.container:hover .title {
+  text-decoration: underline;
+}
+
+.container:last-child {
+  border-bottom: 0;
+}
+
+.info {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 0.75rem;
+}
+
+.title {
+  color: #000;
+  font-size: 1rem;
+  font-weight: 500;
+}
+
+.name {
+  padding-right: 0.5rem;
+
+  color: #777f88;
+  font-size: 0.875rem;
+  font-weight: 500;
+}
+
+.date {
+  padding-left: 0.5rem;
+  border-left: 1px solid rgb(212, 212, 212);
+
+  color: #777f88;
+  font-size: 0.875rem;
+  font-weight: 500;
+}
+
+.viewCount {
+  color: #777f88;
+  font-size: 0.875rem;
+  font-weight: 500;
+}
+
+.viewCount svg {
+  margin-right: 0.25rem;
+}
+</style>
