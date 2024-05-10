04f7cb856735eb9d4c71cb2e91a599cd5e165d7b
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-05-10 17:05:34 +0900

    feat: PostContainer

---
+++ src/components/Post/PostContainer.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.postList">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style module>
+.container {
+}
+
+.postList {
+  box-shadow: 0 0.5rem 2rem -1rem rgba(0, 0, 0, 0.4);
+
+  background-color: #fff;
+}
+</style>
