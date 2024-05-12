79ef958c336fc00fcc2f9ec1665685d1f61c7b34
kimdw524 <kimdw524@naver.com>
2024-05-12 22:13:01 +0900

    feat: Select Component

---
+++ src/components/Common/Select.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+const model = defineModel();
+</script>
+
+<template>
+  <select :class="$style.select" v-model="model">
+    <slot />
+  </select>
+</template>
+
+<style module>
+.select {
+  width: 100%;
+  height: 3rem;
+  padding: 0 0.625rem;
+  box-sizing: border-box;
+  border-radius: 0.175rem;
+  border: 1px solid var(--textfield-border);
+
+  background-color: var(--textfield-bg);
+
+  color: var(--textfield-color);
+  font-size: 1rem;
+
+  transition: all 100ms ease;
+
+  outline: none;
+  cursor: pointer;
+}
+.select:focus {
+  border: 1px solid var(--textfield-border-focus);
+}
+</style>
