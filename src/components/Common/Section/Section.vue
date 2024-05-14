3b12260b5f126a761b42ae5e1ca393b541f0622c
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-05-14 15:01:28 +0900

    feat: Section

---
+++ src/components/Common/Section/Section.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div :class="$style.container">
+    <slot />
+  </div>
+</template>
+
+<style module>
+.container {
+  margin: 0.5rem 0;
+
+  font-size: 1.375rem;
+  font-weight: 500;
+  letter-spacing: 1px;
+}
+
+.container span {
+  color: var(--color-primary);
+  font-weight: 600;
+}
+</style>
---
+++ src/components/Common/Section/SectionDescription.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div :class="$style.container">
+    <slot />
+  </div>
+</template>
+
+<style module>
+.container {
+  margin: 0.5rem 0;
+
+  font-size: 0.875rem;
+  font-weight: 400;
+}
+
+.container span {
+  color: var(--color-primary);
+  font-weight: 600;
+}
+</style>
