5dbc9dfd7efbe5fd18b9ed26d0a5069eb6d1bb88
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-05-14 14:32:20 +0900

    design: Main Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <script setup>
-import Card from '@/components/Common/Card.vue';
+import Card from '@/components/Common/Card/Card.vue';
+import CardTitle from '@/components/Common/Card/CardTitle.vue';
+import CardBody from '@/components/Common/Card/CardBody.vue';
 import MainSearch from '@/components/MainSearch/MainSearch.vue';
+
 import { LottieAnimation } from 'lottie-web-vue';
 import lottieBuilding from '@/assets/lotties/building.json';
 import lottieHouse2 from '@/assets/lotties/house2.json';
@@ -14,21 +17,25 @@ import lottieHouse2 from '@/assets/lotties/house2.json';
 
     <div :class="$style.cardContainer">
       <Card>
-        <div>아파트</div>
+        <CardTitle>아파트</CardTitle>
+        <CardBody>전국에 위치한 모든<br />매물과 단지 정보를<br />확인해 보세요.</CardBody>
         <LottieAnimation
           :animation-data="lottieBuilding"
           :auto-play="true"
-          :loop="false"
+          :loop="true"
           :speed="1"
+          :class="$style.buildingAnimation"
         />
       </Card>
       <Card>
-        <div>연립다세대</div>
+        <CardTitle>연립다세대</CardTitle>
+        <CardBody>주택부터 빌라까지<br />다양한 매물을 찾아보세요.</CardBody>
         <LottieAnimation
           :animation-data="lottieHouse2"
           :auto-play="true"
-          :loop="false"
+          :loop="true"
           :speed="1"
+          :class="$style.houseAnimation"
         />
       </Card>
     </div>
@@ -50,5 +57,23 @@ import lottieHouse2 from '@/assets/lotties/house2.json';
   gap: 1rem;
 
   padding: 1rem;
+}
+
+.buildingAnimation {
+  position: absolute;
+  right: -0.5rem;
+  bottom: -0.5rem;
+  z-index: -10;
+
+  width: 10.5rem;
+}
+
+.houseAnimation {
+  position: absolute;
+  right: -2rem;
+  bottom: -4rem;
+  z-index: -10;
+
+  width: 15rem;
 }
 </style>
